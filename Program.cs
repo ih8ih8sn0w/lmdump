c0275bcc423bbc2655486bcda417d6488f55c08a
ih8ih8sn0w <ih8ih8sn0w@gmail.com>
2017-11-02 12:07:35 -0500

    Forgot to remove crappy debugging checks

--- Program.cs
+++ Program.cs
@@ -15,19 +15,11 @@ namespace lmdump
 			if (args.Length == 0)
 			{
 				lm = new Lumen(@"C:\Users\ih8ih8sn0w\Desktop\SSB4\Tools\Wii U\sm4shexplorer 0.07.1\extract\data\ui\lumen\other\other.lm");
-				Console.WriteLine("Default main.lm loaded");
-				Console.ReadLine();
 			}
 			else
 			{
 				lm = new Lumen(args[0]);
-				Console.WriteLine("This file was loaded: " + args[0]);
-				Console.ReadLine();
 			}
-		}
-		public static string GetPath()
-		{
-			return Convert.ToString(lm);
 		}
 	}
 }
